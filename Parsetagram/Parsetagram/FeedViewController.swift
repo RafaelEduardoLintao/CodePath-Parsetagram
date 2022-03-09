18c58df1ea8787fd2ae7960c44d078347c83b9ff
Rafael Eduardo Lintao <relintao@gmail.com>
2022-03-08 16:08:16 -0800

    Allow pull to refresh and infinite scroll

--- Parsetagram/Parsetagram/FeedViewController.swift
+++ Parsetagram/Parsetagram/FeedViewController.swift
@@ -6,15 +6,85 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    var numberOfPosts: Int!
+    
+    let myRefreshControl = UIRefreshControl()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myRefreshControl.addTarget(self, action: #selector(viewDidAppear), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        numberOfPosts = 20
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = numberOfPosts
+        query.findObjectsInBackground{(posts, error) in
+            if posts != nil{
+                self.posts = posts!
+                self.tableView.reloadData()
+            }//end if
+        }
+    }//end viewDidAppear
+    
+    func loadMorePosts(){
+        let query = PFQuery(className: "Posts")
+        numberOfPosts = numberOfPosts + 5
+        query.includeKey("author")
+        query.limit = numberOfPosts
+        query.findObjectsInBackground{(posts, error) in
+            if posts != nil{
+                self.posts = posts!
+                self.tableView.reloadData()
+            }//end if
+        }
+    }//end loadMorePosts
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath){
+        if indexPath.row + 1 == posts.count{
+            loadMorePosts()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.row] 
+        
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        
+        cell.captionLabel.text = (post["caption"] as! String)
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af.setImage(withURL: url)
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
